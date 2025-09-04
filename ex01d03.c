9b3c0487c7889310e14a731ca84a09a91121986a
Soufyane <elomranisoufyan@gmail.com>
2025-09-04 15:26:25 +0100

    ex01 version 2

--- ex01d03.c
+++ ex01d03.c
@@ -4,7 +4,8 @@
 int main(void) {
     bool digit_seen[10] = {false};
     int digit;
-    long n ;
+    long n  ;
+    
 
     printf("Enter a number: ");
     scanf("%ld", &n);
@@ -16,9 +17,14 @@ int main(void) {
         digit_seen[digit] = true;
         n /= 10;
     }
-
+    for (int i = 0; i < 10; i++) {
+        if (digit_seen[i]) {
+            printf("%d ", i);
+        }
+    }
+  
     if (n > 0) {
-        printf("Repeated digit\n");
+        printf("Repeated digit\n",digit);
     } else {
         printf("No repeated digit\n");
     }
