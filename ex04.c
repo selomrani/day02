137aaec46eb4436155dd17e8240b6416fd660c80
Soufyane <elomranisoufyan@gmail.com>
2025-09-04 11:44:49 +0100

    EX03 - older date

--- ex04.c
+++ ex04.c
@@ -7,36 +7,21 @@ int main (void){
 
     do {
     printf("Enter a date in dd/mm/yy format : ");
-    scanf("%d/%d/%d",dd,mm,yy);
+    scanf("%d/%d/%d",&dd,&mm,&yy);
     }
-    while ( dd < 1 || dd > 31 || mm < 1 || mm > 12 || yy < 0 || yy > 99 );
+    while ( (dd < 1 || dd > 31) || (mm < 1 || mm > 12) && ( yy < 0 || yy > 99) );
 
     do {
     printf("Enter a date in dd/mm/yy format : ");
-    scanf("%d/%d/%d",dd2,mm2,yy2);
+    scanf("%d/%d/%d",&dd2,&mm2,&yy2);
     }
-    while ( dd < 1 || dd > 31 || mm < 1 || mm > 12 || yy < 0 || yy > 99 );
-    
+    while ( (dd < 1  && dd > 31) && (mm < 1 && mm > 12) && ( yy < 0 && yy > 99) );
 
-
-    
-
-
-    
-
-
-    
-
-    
-
-
-
-
-
-
-
-
-
-
-    return 0;
+   if (yy < yy2 || (yy == yy2 && mm < mm2) || (yy == yy2 && mm == mm2 && dd < dd2)) {
+        printf("%02d/%02d/%02d is the older.\n", dd, mm, yy);
+    } else if (yy > yy2 || (yy == yy2 && mm > mm2) || (yy == yy2 && mm == mm2 && dd > dd2)) {
+        printf("%02d/%02d/%02d is the older.\n", dd2, mm2, yy2);
+    } else {
+        printf("You entered the same date twice !.\n");
+    }
 }
